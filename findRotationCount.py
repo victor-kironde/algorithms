29e9cae703ad1b9191cc7c5d920b3ec4564d6026
Victor Kironde <VictorKironde@gmail.com>
2019-02-26 13:48:58 +0300

    refactor

--- findRotationCount.py
+++ findRotationCount.py
@@ -1,20 +1,21 @@
 def findRotationCount(arr):
-  left = 0
-  right = len(arr) - 1
-  while (left <= right):
-    if arr[left] <= arr[right]:
-      return left
-    mid = int((left + right)/2)
+  start = 0
+  end = len(arr) - 1
+  while (start <= end):
+    if arr[start] <= arr[end]:
+      return start
+    mid = int((start + end)/2)
     next = (mid + 1)
     prev = (mid -1)
-    print(prev, next)
+
 
     if arr[mid] <= arr[next] and arr[mid] <= arr[prev]:
       return mid
-    elif arr[mid] <= arr[right]:
-      right = mid - 1
-    elif arr[mid] >= arr[left]:
-      left = mid + 1
+    #find the sorted side in order to drop it.
+    elif arr[mid] <= arr[end]:
+      end = mid - 1
+    else:
+      start = mid + 1
   return -1
 
 # arr = [8, 9, 10, 1, 2, 3, 4, 5, 6, 7]
